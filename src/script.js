3e3d8f0cd05e0366ad885bba4f53054d397f82cc
vivialyn14 <vivialyn14@gmail.com>
2025-11-14 12:28:57 +0000

    Added search engine & API to update city, country, temperature, wind & humidity,

--- src/script.js
+++ src/script.js
@@ -1,9 +1,53 @@
 function newCity(event) {
+  //prevent page reload
   event.preventDefault();
+
+  // change h2 to searched city
   let searchInput = document.querySelector("#search-bar");
-  console.log(searchInput.value);
+
+  // change temp-value to current temp according to API
+  let apiKey = "1dd3bcb8741dt0aoe047389dbb6b5df4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
+
+  function getWeather(response) {
+    //change temperature
+    let tempData = Math.round(response.data.temperature.current);
+    let tempValue = document.querySelector("#temp-value");
+    tempValue.innerHTML = tempData;
+    //change city & country
+    let cityName = document.querySelector("#city-name");
+    cityName.innerHTML = response.data.city;
+    let countryName = document.querySelector("#country");
+    //countryName.innerHTML = response.data.country;
+    let shortCountry = {
+      "United Kingdom of Great Britain and Northern Ireland": "UK",
+      "United States of America": "USA",
+      "Russian Federation": "Russia",
+      "Congo (Democratic Republic of the)": "DRC",
+    };
+    if (response.data.country in shortCountry) {
+      countryName.innerHTML = shortCountry[response.data.country];
+    } else {
+      countryName.innerHTML = response.data.country;
+    }
+
+    //change description
+    let description = document.querySelector("#weather-description");
+    description.innerHTML = response.data.condition.description;
+    //change details
+    let windSpeed = document.querySelector("#wind-speed");
+    let humidity = document.querySelector("#humidity");
+    windSpeed.innerHTML = `${Math.round(response.data.wind.speed)} m/s`;
+    humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  }
+
+  axios.get(apiUrl).then(getWeather);
 }
 
 let searchForm = document.querySelector("#search-form");
-console.log(searchForm.value);
-//searchForm.addEventListener("submit", newCity);
+searchForm.addEventListener("submit", newCity);
+
+// click submit to:
+//   search for a new city
+//   change h2 to searched city
+//   change temp-value to current temp according to API
